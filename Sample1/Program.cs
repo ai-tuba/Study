0cb2da4047942e4d1d7cbca00fc633c9f0d9b5c1
aikanakamura <tea.jack.cat15@gmail.com>
2014-11-18 00:49:24 +0900

    テスト xとyの値変更

---
+++ Sample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sample
+{
+    class Point2D
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", x, y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Point2D { x = 5, y = 6 };
+            Console.WriteLine(p);
+        }
+    }
+}
